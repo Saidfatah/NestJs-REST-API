e34cef0df29be179f846da06de46a77a083ee349
SaidFatah <said_designer@outlook.com>
2022-09-16 14:45:47 +0100

    removed response handling stuff

--- src/coffes/coffes.controller.ts
+++ src/coffes/coffes.controller.ts
@@ -1,28 +1,23 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffes')
 export class CoffesController {
   @Get()
   findAll() {
-    // response.status(200).send(JSON.stringify(['espresso', 'latte']));
     return ['espresso', 'latte'];
   }
   @Get('flavors')
   findAllFlavors() {
     return ['choclate', 'vannila'];
   }
-  //   @Get(':id')
-  //   findOne(@Param() params) {
-  //     return params.id;
-  //   }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return 'id' + id;
   }
 
-  //@HttpCode(HttpStatus.GONE)
   @Post()
-  brewCoffe(@Body("type") type:string) {
+  brewCoffe(@Body('type') type: string) {
     console.log(type);
     return type;
   }

7c3427f6a1ecd629895a8e8c5480e5259c5e433f
SaidFatah <said_designer@outlook.com>
2022-09-17 12:49:11 +0100

    added put ,patch and delete handlers

--- src/coffes/coffes.controller.ts
+++ src/coffes/coffes.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('coffes')
 export class CoffesController {
@@ -17,8 +26,32 @@ export class CoffesController {
   }
 
   @Post()
-  brewCoffe(@Body('type') type: string) {
+  brew(@Body('type') type: string) {
     console.log(type);
     return type;
+  }
+
+  //PATCH is a method of modifying resources where the client
+  //sends partial data that is to be updated without modifying the entire data.
+  @Patch(':id')
+  updateFlavor(@Param('id') id: string, @Body() body) {
+    console.log({ id });
+    console.log(body);
+    return 'this partially updates the resource: ' + id;
+  }
+
+  //PUT is a method of modifying resources where the client
+  //sends data that updates the entire resource
+  //PUT is similar to POST in that it can create resources,
+  //but it does so when there is a defined URL wherein PUT
+  //replaces the entire resource if it exists or creates new if it does not exist.
+  @Put(':id')
+  updateFlavorPut(@Param('id') id: string, @Body() body) {
+    return 'this updates the whole resource at :' + id;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return 'this deletes resource :' + id;
   }
 }
